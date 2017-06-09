224a8309ccb7752a4186bdb189ac2491e071e110
Sergii <sergii.bondarenko.triare@gmail.com>
2017-06-09 17:47:07 +0300

    add validation to mail

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,10 +1,23 @@
-//(function ($) {
+var emailList = [];
+//console.log(emailList);
+function validateForm() {
+    var x = document.forms["myForm"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
 
-   // $(window).load(function () {
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+       console.log("Not a valid e-mail address");
+        return false;
+    }else{
+       alert("yes");
+       alert(x);
 
- //   });
 
-//});
+
+    }
+}
+
+
 
 $('#anim').fadeIn(1500) //wait3 1.5 s.
     .show(1000);
