bf90515eb1d219fa8c6bee6bafeb4326e9f01a56
Sergii <sergii.bondarenko.triare@gmail.com>
2017-06-10 12:49:46 +0300

    change logo

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -2,10 +2,29 @@
 $('#anim').fadeIn(1500) //wait3 1.5 s.
     .show(1000);
 
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+               // alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+var emailList =  readTextFile("emailList.txt");
+//alert(ss);
 
 
-var emailList = [];
-//console.log(emailList);
+var emails = [];
+console.log(emails);
 function validateForm() {
     var x = document.forms["myForm"]["email"].value;
     var atpos = x.indexOf("@");
@@ -15,13 +34,16 @@ function validateForm() {
        console.log("Not a valid e-mail address");
         return false;
     }else{
-        console.log("yes");
-        console.log(x);
 
+        var email =  document.getElementsByTagName("input")[0].value;
+        var emails = [];
+        emails.push(email);
+        console.log(emails);
+        alert(email);
+        alert(emails);
 
 
     }
 }
-
 
 
